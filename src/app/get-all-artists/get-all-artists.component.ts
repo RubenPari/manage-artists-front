e4e4ed8999b0bceeb49538fdfed61a9fe231ce58
RubenPari <rubenpari.developer@gmail.com>
2022-07-29 17:43:50 +0200

    created get-all-artist component

---
+++ src/app/get-all-artists/get-all-artists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-artists',
+  templateUrl: './get-all-artists.component.html',
+  styleUrls: ['./get-all-artists.component.scss']
+})
+export class GetAllArtistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
