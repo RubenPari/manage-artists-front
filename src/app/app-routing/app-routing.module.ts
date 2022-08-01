8f0dc5b9b68a57f1f84a781c998adad73c948f20
RubenPari <rubenpari.developer@gmail.com>
2022-08-01 17:00:22 +0200

    update router and implemented basic temp for add-artist

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from '../app-module/app.component';
 import { GetAllArtistsComponent } from '../get-all-artists/get-all-artists.component';
+import { AddArtistComponent } from "../add-artist/add-artist.component";
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'get-all-artists', component: GetAllArtistsComponent },
+  { path: 'add-artist', component: AddArtistComponent },
 ];
 
 @NgModule({
