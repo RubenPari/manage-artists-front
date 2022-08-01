f52dbc5ef4cc1893ba8e3a73afb1f4e74910c49f
RubenPari <rubenpari.developer@gmail.com>
2022-08-01 11:52:04 +0200

    added add-artist component

--- src/app/app-module/app.module.ts
+++ src/app/app-module/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './../app-routing/app-routing.module';
+import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './../navbar/navbar.component';
-import { GetAllArtistsComponent } from './../get-all-artists/get-all-artists.component';
-import { SyncPlaylistComponent } from './../sync-playlist/sync-playlist.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { GetAllArtistsComponent } from '../get-all-artists/get-all-artists.component';
+import { SyncPlaylistComponent } from '../sync-playlist/sync-playlist.component';
+import {AddArtistComponent} from "../add-artist/add-artist.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     GetAllArtistsComponent,
+    AddArtistComponent,
     SyncPlaylistComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
