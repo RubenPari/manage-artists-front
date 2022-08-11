0d20e87725da79834ebf9fc731b1cd0283574a05
RubenPari <rubenpari.developer@gmail.com>
2022-08-11 09:14:15 +0200

    changed name project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/feat-eminem-front'),
+      dir: require('path').join(__dirname, './coverage/manage-artists-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
